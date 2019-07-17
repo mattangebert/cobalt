0973f41e5f628aaf0c662f9d403e8951309c4985
mattan.gebert <mattan.gebert@flagbit.de>
2019-07-17 11:19:23 +0200

    [master] Fix & Adjust option control functions test

--- src/app/form-option/service/option-control-functions.spec.ts
+++ src/app/form-option/service/option-control-functions.spec.ts
@@ -9,10 +9,23 @@ describe('toFormGroup', () => {
       label: 'Test 1',
       type: 'number',
       value: 100,
-      require: false,
+      required: false,
       min: 10,
       max: 600,
       order: 3
+    });
+
+    const form = toFormGroup([option]);
+
+    expect(form).toBeTruthy();
+  });
+
+  it('should be created with no value set', () => {
+    const option = new OptionInput({
+      key: 'test2',
+      label: 'Test 2',
+      type: 'text',
+      required: false,
     });
 
     const form = toFormGroup([option]);
@@ -25,8 +38,21 @@ describe('toFormGroup', () => {
       key: 'test2',
       label: 'Test 2',
       type: 'text',
-      value: '',
-      require: true,
+      value: 'blub',
+      required: true,
+    });
+
+    const form = toFormGroup([option]);
+
+    expect(form).toBeTruthy();
+  });
+
+  it('should be created (required) with no value set', () => {
+    const option = new OptionInput({
+      key: 'test2',
+      label: 'Test 2',
+      type: 'text',
+      required: true,
     });
 
     const form = toFormGroup([option]);
